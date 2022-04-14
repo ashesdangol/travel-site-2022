66f4a81ddf09e063b13680ba6f7391d229b04d1e
Ashes <ashesdangol@hotmail.com>
2022-04-14 23:23:44 +1000

    Module Only Active link

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import "../styles/styles.css"
 import MobileMenu from "./modules/MobileMenu"
 import RevealOnScroll from "./modules/RevealOnScroll"
 import StickyHeader from "./modules/StickyHeader"
+import ActiveLink from "./modules/ActiveLink"
 
 if (module.hot) {
   module.hot.accept()
@@ -10,4 +11,7 @@ if (module.hot) {
 let mobileMenu = new MobileMenu()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 80)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 85)
-let stickyHeader = new StickyHeader(document.querySelector(".site-header"), 60)
+/* let stickyHeader = new StickyHeader(document.querySelector(".site-header"), 60) */
+
+/* Active Link */
+let activeLink = new ActiveLink(document.querySelectorAll(".page-section"))
