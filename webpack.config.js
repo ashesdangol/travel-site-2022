fb4b4f74543f1ab2491235c1d8817eac47750814
Ashes <ashesdangol@hotmail.com>
2022-04-15 16:39:10 +1000

    copy html and images and use of fse

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const currenTask = process.env.npm_lifecycle_event
 const postCSSPlugins = [require("postcss-mixins"), require("postcss-pxtorem")({ propWhiteList: [] }), require("postcss-import"), require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
 
+class RunAfterComplile {
+  apply(compiler) {
+    compiler.hooks.done.tap("copy images", function () {
+      fse.copySync("./app/assets/images", "./dist/assets/images")
+    })
+  }
+}
+
 const { watch } = require("fs")
 const path = require("path")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
@@ -8,6 +16,7 @@ const MiniCSSExtractPlugin = require("mini-css-extract-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const fse = require("fs-extra")
 
 let cssConfig = {
   test: /\.css$/i,
@@ -23,10 +32,21 @@ let cssConfig = {
     }
   ]
 }
+let pages = fse
+  .readdirSync("./app")
+  .filter(function (file) {
+    return file.endsWith(".html")
+  })
+  .map(function (page) {
+    return new HtmlWebpackPlugin({
+      filename: page,
+      template: `./app/${page}`
+    })
+  })
 
 let config = {
   entry: "./app/assets/scripts/App.js",
-  plugins: [new HtmlWebpackPlugin({ filename: "index.html", template: "./app/index.html" })],
+  plugins: pages,
   module: {
     rules: [cssConfig]
   }
@@ -64,7 +84,7 @@ if (currenTask == "build") {
     minimize: false,
     minimizer: [`...`, new CssMinimizerPlugin()]
   }
-  config.plugins.push(new CleanWebpackPlugin(), new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" }))
+  config.plugins.push(new CleanWebpackPlugin(), new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" }), new RunAfterComplile())
 }
 
 module.exports = config
