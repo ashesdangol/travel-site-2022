9c4f1d5f06bbfc69371fd193c8f30041c6982317
Ashes <ashesdangol@hotmail.com>
2022-04-15 13:34:36 +1000

    part1- build process for dev and production

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,11 @@
+const currenTask = process.env.npm_lifecycle_event
 const postCSSPlugins = [require("postcss-mixins"), require("postcss-pxtorem")({ propWhiteList: [] }), require("postcss-import"), require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
 
 const { watch } = require("fs")
 const path = require("path")
-module.exports = {
+
+let config = {
   entry: "./app/assets/scripts/App.js",
-  output: {
-    filename: "bundled.js",
-    path: path.resolve(__dirname, "app")
-  },
-  devServer: {
-    watchFiles: ["./app/**/*.html"],
-    static: {
-      directory: path.join(__dirname, "app"),
-      watch: false
-    },
-    hot: true,
-    port: 3000,
-    host: "0.0.0.0",
-    allowedHosts: "auto"
-  },
-  mode: "development",
   module: {
     rules: [
       {
@@ -40,3 +26,31 @@ module.exports = {
     ]
   }
 }
+
+if (currenTask == "dev") {
+  config.output = {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app")
+  }
+  config.devServer = {
+    watchFiles: ["./app/**/*.html"],
+    static: {
+      directory: path.join(__dirname, "app"),
+      watch: false
+    },
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0",
+    allowedHosts: "auto"
+  }
+  config.mode = "development"
+}
+if (currenTask == "build") {
+  config.output = {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "dist")
+  }
+  config.mode = "production"
+}
+
+module.exports = config
