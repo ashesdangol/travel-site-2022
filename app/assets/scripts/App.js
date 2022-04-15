6abf5a2771eaaf3697d8a70d4c1c03b591b4a273
Ashes <ashesdangol@hotmail.com>
2022-04-15 10:06:27 +1000

    open and close the modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import "../styles/styles.css"
 import MobileMenu from "./modules/MobileMenu"
 import RevealOnScroll from "./modules/RevealOnScroll"
 import StickyHeader from "./modules/StickyHeader"
+import Modal from "./modules/Modal"
 
 if (module.hot) {
   module.hot.accept()
@@ -11,3 +12,4 @@ let mobileMenu = new MobileMenu()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 80)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 85)
 let stickyHeader = new StickyHeader(document.querySelector(".site-header"), 60)
+new Modal()
