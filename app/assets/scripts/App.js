53d235952376fe001f23e8999bbfbc02df0cf3b6
Ashes <ashesdangol@hotmail.com>
2022-04-15 10:54:43 +1000

    complete lazy loading

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,8 @@
 import "../styles/styles.css"
+import "lazysizes"
 import MobileMenu from "./modules/MobileMenu"
 import RevealOnScroll from "./modules/RevealOnScroll"
 import StickyHeader from "./modules/StickyHeader"
-import Modal from "./modules/Modal"
 
 if (module.hot) {
   module.hot.accept()
